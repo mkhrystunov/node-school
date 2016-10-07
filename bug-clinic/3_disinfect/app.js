150651fd0f4b9e97d679f2a6588f8d3b0ee10df7
Maksym Khrystunov <mkhristunov@experty.com>
2016-10-07 15:27:17 +0200

    bug-clinic. 3rd task

---
+++ bug-clinic/3_disinfect/app.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* jshint ignore: start */
+var today = "today";
+
+function timestamp() {
+    today = new Date();
+    return today;
+}
+
+console.log("date is", timestamp());
+console.log("today is", today);
