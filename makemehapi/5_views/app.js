ef61ffe1fd57865615f9e5023e7a6ab06384bcaa
Maksym Khrystunov <mkhristunov@experty.com>
2016-10-10 16:34:44 +0200

    makemehapi. 5th task

---
+++ makemehapi/5_views/app.js
@@ -0,0 +1,35 @@
+let path = require('path');
+let Hapi = require('hapi');
+let Vision = require('vision');
+
+let server = new Hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
+
+server.register(Vision, (err) => {
+    if (err) {
+        throw err;
+    }
+});
+
+server.views({
+    engines: {
+        html: require('handlebars')
+    },
+    path: path.join(__dirname, 'templates')
+});
+
+server.route({
+    path: '/',
+    method: 'GET',
+    handler: {
+        view: 'index.html'
+    }
+});
+
+server.start(() => {
+    console.log('Server running at: %s', server.info.uri);
+});
