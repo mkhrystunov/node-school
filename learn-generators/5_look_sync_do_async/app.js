5e11fad822dfad98af51a603e416e8161deb90a6
Maksym Khrystunov <mkhristunov@experty.com>
2016-10-17 11:54:16 +0200

    learn-generators. 5th task

---
+++ learn-generators/5_look_sync_do_async/app.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+
+function run(generator) {
+    var it = generator(go);
+
+    function go(err, result) {
+        if (err) {
+            return it.throw(err);
+        }
+        it.next(result);
+    }
+
+    go();
+}
+
+run(function*(done) {
+    var firstFile;
+    try {
+        var dirFiles = yield fs.readdir('NoNoNoNo', done);
+        firstFile = dirFiles[0];
+    } catch(e) {
+        firstFile = null;
+    }
+
+    console.log(firstFile);
+});
