d319485b8e760abbeae9d0db38e9901af5e2f3f1
Maksym Khrystunov <mkhristunov@experty.com>
2016-10-17 14:51:35 +0200

    learnyoureact. 6th lesson

---
+++ learnyoureact/6_css/views/index.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+let style = {
+    table: {
+        border: '2px solid black'
+    },
+    tableContent: {
+        border: '1px solid black'
+    }
+};
+
+export default class TodoBox extends React.Component {
+    render() {
+        return (
+            <div className="todoBox">
+                <h1>Todos</h1>
+                <TodoList />
+                <TodoForm />
+            </div>
+        );
+    }
+}
+
+class TodoList extends React.Component {
+    render() {
+        return (
+            <div className="todoList">
+                <table style={style.table}>
+                    <tbody>
+                    <Todo title="Shopping">Milk</Todo>
+                    <Todo title="Hair cut">13:00</Todo>
+                    <Todo title="Learn React">15:00</Todo>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+class Todo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            checked: false
+        };
+    }
+
+    handleChange(e) {
+        this.setState({
+            checked: e.target.checked
+        });
+    }
+
+    render() {
+        return (
+            <tr>
+                <td style={style.tableContent}>
+                    <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
+                </td>
+                <td style={style.tableContent}>{this.props.title}</td>
+                <td style={style.tableContent}>{this.props.children}</td>
+            </tr>
+        );
+    }
+}
+Todo.propTypes = {
+    title: React.PropTypes.string.isRequired
+};
+
+class TodoForm extends React.Component {
+    render() {
+        return (
+            <div className="todoForm">
+                I am a TodoForm.
+            </div>
+        );
+    }
+}
