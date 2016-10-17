00f7bd240f53c18a57a42b703eb1a2602f3fab05
Maksym Khrystunov <mkhristunov@experty.com>
2016-10-17 11:10:08 +0200

    planetproto. 6th task

---
+++ planetproto/060.js
@@ -0,0 +1,16 @@
+/* global claim */
+var machine = {};
+
+var robot = Object.create(machine);
+
+var robby = Object.create(robot);
+
+claim(machine.isPrototypeOf(robby), true);
+claim(robot.isPrototypeOf(robby), true);
+claim.same(Object.getPrototypeOf(robby), robot);
+
+module.exports = {
+    machine: machine,
+    robot: robot,
+    robby: robby
+};
